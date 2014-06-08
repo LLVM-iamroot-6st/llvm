71e7a7b3582a162cf662c5975054279b74cf21bc
eundoo song <eundoo.song@gmail.com>
2014-06-08 17:41:25 +0900

    20140607 : LLVM Tutorial ch 6 done

---
+++ study/20140608/test_precedence_calc.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <map>
+
+/*
+Author : eundoo.song
+
+This simple caculator provides an example of Operator-precedence_parser.
+reference : 1) http://en.wikipedia.org/wiki/Operator-precedence_parser
+			2) http://llvm.org/docs/tutorial/LangImpl2.html
+
+But this is Not fully implemented caculator, meaning that it runs as very limited one.
+1. It can't parse any number that has more than two digit number and any sub-expression.
+   So, all operands must be [0-9]
+   1+2*4-2 --> OK
+   1+22*44-2 --> NO!!
+2. Only "+, -, *, /" supported.
+3. It doesn't handle all possible errors.
+
+Please improve me! :)
+
+*/
+class calc
+{
+private:
+	char CurTok;// array index of Current Token
+	std::map<char, int> op_precedence;
+	std::string Input;
+
+	int binaryCalc(int exprPrec, int LHS)
+	{
+		while(1)
+		{
+			int TokPrec = GetOpPrecedence();
+
+			if(TokPrec < exprPrec)
+			{
+				return LHS;
+			}
+
+			char OP = Input[CurTok++];
+			int RHS = Input[CurTok++] - 48;
+
+			int NextPrec = GetOpPrecedence();
+			if(TokPrec < NextPrec)
+			{
+				RHS = binaryCalc(TokPrec+1, RHS);			
+			} 
+
+			std::cout<<"RHS:"<<RHS<<std::endl; 
+			std::cout<<"OP:"<<OP<<std::endl; 
+			std::cout<<"LHS:"<<LHS<<"\n"<<std::endl; 
+
+			LHS = DoCalc(OP,LHS,RHS);
+ 
+		}
+	}
+
+	int DoCalc(char OP, int LHS, int RHS)
+	{
+		switch(OP)
+		{
+			case '+':
+				return LHS + RHS;		
+			case '-':
+				return LHS - RHS;		
+			case '*':
+				return LHS * RHS;		
+			case '/':
+				return LHS / RHS;
+			defualt:
+				return -1;		
+		}					
+	}
+
+	int GetOpPrecedence()
+	{
+		if(CurTok >= Input.size())
+		{
+			return -1;
+		}
+
+		if(op_precedence[Input[CurTok]] > 0)
+		{
+			return op_precedence[Input[CurTok]]; 
+		}
+
+		return -1;
+	}
+
+public:
+	calc()
+	{
+		op_precedence['+'] =  20;
+		op_precedence['-'] =  20;
+		op_precedence['*'] =  30;
+		op_precedence['/'] =  30;
+	}
+
+	int DoCalc(const std::string& input)
+	{
+		Input = input;	
+		CurTok = 1;
+		binaryCalc(0, input[0] - 48);
+	}
+};
+
+
+int main(void)
+{
+	std::string input;
+	std::cin>>input;
+
+	calc c;
+	std::cout<<c.DoCalc(input);
+	return 0;
+}
+
